39b91e5a211a52434c777befb8f5038df0d66abc
dwoodhouse1 <148891593+dwoodhouse1@users.noreply.github.com>
2023-12-19 13:12:24 +0000

    Slick added
    Slick Plugin for banners added

---
+++ js/banners.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// JavaScript Banners
+// ==========================================================================
+$(document).ready(function(){
+    $('.carousell-clients-content').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        variableWidth: true,
+        //prevArrow: false,
+        //nextArrow: false,
+        draggable: false
+    });
+});
