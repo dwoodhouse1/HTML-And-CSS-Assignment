521e80da748dfa97d997ceb18a321603ce1065ae
dwoodhouse1 <148891593+dwoodhouse1@users.noreply.github.com>
2024-01-24 15:38:05 +0000

    Mobile Viewport HTML & CSS for contact-us.php
    Added all HTML + CSS required for contact-us.php for the mobile screen size.

--- contact-us.php
+++ contact-us.php
@@ -20,24 +20,160 @@
             <div class="main-outer">
                 <div class="main-inner">
                     <?php include("php/header.php"); ?> <!-- Header Content -->
+                    <div class="hidden-xs breadcrumb-container">
+                        <div class="container">
+                            <ul class="breadcrumb">
+                                <li><a>Home</a></li>
+                                <li>Our Offices</li>
+                            </ul>
+                        </div>
 
+                    </div>
                     <div class="page-head">
-                        <h1>Our Offices</h1>
+                        <div class="container">
+                            <h1>Our Offices</h1>
+                        </div>
+                        
                     </div>
 
                     <div class="office-addresses">
-                        <h2>Office Location cards will go here TEHE</h2>
+                        <div class="container">
+
+                            <div class="block address address-london">
+                                <div class="location-image">
+                                    <a><img src="img/london.jpg" alt="London Office" class="img-office"></a>
+                                </div>
+                                <div class="location-content">
+                                    <p class="h2"><a>London Office</a></p>
+                                    <p class="p">Unit G6,
+                                        <br>
+                                        Pixel Business Centre
+                                        <br>
+                                        110 Brooker Road, Waltham Abbey,
+                                        <br>
+                                        London
+                                        <br>
+                                        EN9 1JH
+                                    </p>
+                                    <div class="tel">
+                                        <a class="h3 text-web">02045 397354</a>
+                                    </div>
+                                    <div class="view-more">
+                                        <a class="btn btn-web">View More</a>
+                                    </div>
+                                </div>
+                                <div class="map">
+                                    <img src="img/london-loc.jpeg" class="img-office-location">
+                                </div>
+                            </div>
+
+                            <div class="block address address-cambridge">
+                                <div class="location-image">
+                                    <a><img src="img/london.jpg" alt="Cambridge Office" class="img-office"></a>
+                                </div>
+                                <div class="location-content">
+                                    <p class="h2"><a>Cambridge Office</a></p>
+                                    <p class="p">Unit 1.31,
+                                        <br>
+                                        St John's Innovation Centre,
+                                        <br>
+                                        Cowley Road, Milton,
+                                        <br>
+                                        Cambridge
+                                        <br>
+                                        EN9 1JH
+                                    </p>
+                                    <div class="tel">
+                                        <a class="h3 text-web">01223 37 57 72</a>
+                                    </div>
+                                    <div class="view-more">
+                                        <a class="btn btn-web">View More</a>
+                                    </div>
+                                </div>
+                                <div class="map">
+                                    <img src="img/cambridge-loc.jpeg" class="img-office-location">
+                                </div>
+                            </div>
+
+                            <div class="block address address-wymondham">
+                                <div class="location-image">
+                                    <a><img src="img/wymondham.jpg" alt="Wymondham Office" class="img-office"></a>
+                                </div>
+                                <div class="location-content">
+                                    <p class="h2"><a>Wymondham Office</a></p>
+                                    <p class="p">Unit 15,
+                                        <br>
+                                        Penfold Drive,
+                                        <br>
+                                        Gateway 11 Business Park,
+                                        <br>
+                                        Wymondham, Norfolk,
+                                        <br>
+                                        NR18 0WZ
+                                    </p>
+                                    <div class="tel">
+                                        <a class="h3 text-web">01603 70 40 20</a>
+                                    </div>
+                                    <div class="view-more">
+                                        <a class="btn btn-web">View More</a>
+                                    </div>
+                                </div>
+                                <div class="map">
+                                    <img src="img/wymondham-loc.jpeg" class="img-office-location">
+                                </div>
+                            </div>
+
+                            <div class="block address address-yarmouth">
+                                <div class="location-image">
+                                    <a><img src="img/yarmouth-2.jpg" alt="Great Yarmouth Office" class="img-office"></a>
+                                </div>
+                                <div class="location-content">
+                                    <p class="h2"><a>Great Yarmouth Office</a></p>
+                                    <p class="p">Suite F23,
+                                        <br>
+                                        Beacon Innovation Centre,
+                                        <br>
+                                        Beacon Park, Gorleston,
+                                        <br>
+                                        Great Yarmouth, Norfolk,
+                                        <br>
+                                        NR31 7RA
+                                    </p>
+                                    <div class="tel">
+                                        <a class="h3 text-web">01493 60 32 04</a>
+                                    </div>
+                                    <div class="view-more">
+                                        <a class="btn btn-web">View More</a>
+                                    </div>
+                                </div>
+                                <div class="map">
+                                    <img src="img/yarmouth-loc.jpeg" class="img-office-location">
+                                </div>
+                            </div>
+                            
+                        </div>
+                       
                     </div>
                     <div class="container section">
                         <div class="row">
                             <div class="netmatters-info">
                                 <p>Email us on:</p>
-                                <p><a class="h3">sales@netmatters.com</a></p> <!-- May need to use mailto:sales@netters.com attribute -->
-                                <p><strong>Business Hours</strong></p>
+                                <p><a class="h3 text-web">sales@netmatters.com</a></p> <!-- May need to use mailto:sales@netters.com attribute -->
+                                <p><strong>Business Hours:</strong></p>
                                 <p><strong>Monday - Friday 07:00 - 18:00&nbsp;</strong></p>
                                <!-- container class for dropdown-content class will go here -->
                                 <div class="dropdown-content">
-                                    <h4><a>Out of Hours IT Support</a></h4>
+                                    <h4><a>Out of Hours IT Support&nbsp;</a><span class="icon-angle-double-right"></h4> <!-- Need a new icon here -->
+                                    <div class="dropdown-expand"> <!-- Section needs JS to expand / collapse -->
+                                        <p>Netmatters IT are offering an Out of Hours service for Emergency and Critical tasks.</p>
+                                        <p>
+                                            <strong>Monday - Friday 18:00 - 22:00</strong>
+                                            <strong>Saturday 08:00 - 16:00</strong>
+                                            <br>
+                                            <strong>Sunday 10:00 - 18:00</strong>
+                                        </p>
+                                        <p>To log a critical task, you will need to call our main line number and select Option 2 to leave an Out of Hours&nbsp; voicemail. A technician will contact you on the number provided within 45 minutes of your call.&nbsp;</p>
+                                    </div>
                                 </div>
                             </div>
                             <div class="form-enquiry">
